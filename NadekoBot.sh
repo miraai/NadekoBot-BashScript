f3612632ebd32771e921ee3a777af25c06d26956
miraai <ajla.dzekovic.2470@metropolitan.ac.rs>
2016-10-18 14:32:28 +0200

    Add files via upload

---
+++ NadekoBot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+  echo "NadekoBot 1.0 Download"
+  echo "Mirai was here."
+  mkdir NadekoBot-temp
+  echo "Created NadekoBot-temp folder"
+  cd temp
+  echo "Downloading NadekoBot, please wait."
+  git clone -b 1.0 --recursive -v https://github.com/Kwoth/NadekoBot.git
+  echo "NadekoBot downloaded."
+exit 0
