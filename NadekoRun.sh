a1f1cd59209caf56190d8fac16c6b3cf08a559a0
miraai <ajla.dzekovic.2470@metropolitan.ac.rs>
2016-10-22 21:49:46 +0200

    Update NadekoRun.sh

--- NadekoRun.sh
+++ NadekoRun.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
   echo "NadekoBot 1.0"
   echo "Mirai was here."
-  if git --version 2>&1 >/dev/null; 
+  if git 2>&1 >/dev/null; 
   then echo "Git Installed." 
   else echo "Git is not installed. Please install Git." | exit 1
-  if dotnet --version 2>&1 >/dev/null; 
+  if dotnet 2>&1 >/dev/null; 
   then echo "Dotnet installed." 
   else echo "Dotnet is not installed. Please install dotnet." | exit 1
   cd NadekoBot/src/NadekoBot
