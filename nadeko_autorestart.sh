9b0899786a7201bc60cf9524885d26d1e61c9104
samvaio <samdivaio@icloud.com>
2016-12-09 18:07:27 +0530

    Update nadeko_autorestart.sh

--- nadeko_autorestart.sh
+++ nadeko_autorestart.sh
@@ -12,13 +12,14 @@ echo ""
 echo "Welcome to NadekoBot Auto Restart and Update!"
 
 choice=5
-	echo "1. Run Auto Restart without Updating.
+	echo "1. Run Auto Restart without Updating."
 	echo "2. Auto Restart and Update with Dev Build (latest)"
 	echo "3. Auto Restart and Update with Stable Build"
 	echo "4. Exit"
 	echo -n "Choose 1 for runing NadekoBot to auto restart on ".die" command without updating itself, [2 or 3] to Run with Auto Updating and Restarting after using ".die" command."
 while [ $choice -eq 5 ]; do
 read choice
+
 if [ $choice -eq 1 ] ; then
 	echo ""
 	echo "
@@ -52,7 +53,7 @@ else
 				echo "Exiting..."
 				exit 0
 			else
-				echo "1. Run Auto Restart without Updating.
+				echo "1. Run Auto Restart without Updating."
 				echo "2. Auto Restart and Update with Dev Build (latest)"
 				echo "3. Auto Restart and Update with Stable Build"
 				echo "4. Exit"
