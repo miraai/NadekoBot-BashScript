7ac7f9a0902080a28066d7b40c0fc3fc56815f8a
samvaio <samdivaio@icloud.com>
2016-12-09 18:19:05 +0530

    Create NadekoARU_Latest.sh

---
+++ NadekoARU_Latest.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo ""
+echo "Running NadekoBot with auto restart and updating to latest build!"
+sleep 5s
+while :; do cd NadekoBot/src/NadekoBot && dotnet run -c Release && cd - && curl -L https://github.com/Kwoth/NadekoBot-BashScript/raw/master/nadeko_installer_latest.sh | sh; sleep 5s; done
+echo ""
+echo "That didn't work? Please report in #NadekoLog Discord Server."
+sleep 3s
+bash linuxAIO.sh
+echo "Done"
+exit 0
