2b2b0af867a2666c62e284b8080da2bcac1ddd73
The Oddball <oddball@oddball.tf>
2017-01-25 22:23:15 -0700

    Changed stuff with echos.

--- linuxAIO.sh
+++ linuxAIO.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo ""
-echo "Welcome to NadekoBot."
+echo "Welcome to NadekoBot. If you ran the Auto-Installer, you might want to exit and configure your credentials.json."
 
 choice=6
 	echo "1. Download Dev Build (latest)"
@@ -17,7 +17,7 @@ if [ $choice -eq 1 ] ; then
 	echo "Downloading NadekoBot, please wait."
 	curl -L https://github.com/Kwoth/NadekoBot-BashScript/raw/master/nadeko_installer_latest.sh | sh
 	echo ""
-	echo "NadekoBot Dev Build (latest) downloaded."
+	echo "NadekoBot Dev Build (Latest) downloaded."
 	bash linuxAIO.sh
 else
 	if [ $choice -eq 2 ] ; then
@@ -49,12 +49,12 @@ else
 					echo "Exiting..."
 					exit 0
 				else
-					echo "1. Download Dev Build (latest)"
+					echo "1. Download Dev Build (Latest)"
 					echo "2. Download Stable Build"
-					echo "3. Run Nadeko (normally)"
-					echo "4. Run Nadeko with Auto Restart (check if nadeko is working properly, before using this)"
+					echo "3. Run Nadeko (Normally)"
+					echo "4. Run Nadeko with Auto Restart (Run Nadeko normally before using this.)"
 					echo "5. To exit"
-					echo -n "Choose [1 or 2] to Download [3 or 4] to Run."
+					echo -n "Choose [1 or 2] to Download, [3 or 4] to Run, or [5] to Exit."
 					choice=6
 				fi
 			fi
